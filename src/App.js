44f8bd8488e6aa5286e1afe0b8ad694922a5e23d
Lex <nxlex87@gmail.com>
2023-11-01 13:29:26 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,75 +1,80 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Add from './components/Add/Add';
 import List from './components/List/List';
-import {Context} from './context'
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const local = JSON.parse(localStorage.getItem('tasks') || '[]') 
-    setTasks(local)
-  }, [])
+    const local = JSON.parse(localStorage.getItem('tasks') || '[]');
+    setTasks(local);
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-  }, [tasks])
-	
-  const handleAddTd = data => {
-    const newTasks = [data, ...tasks]
-    setTasks(newTasks)
-  }  
-  const handleDelTd = id => {
-    setTasks(prev => prev.filter(item => item.id !== id))
-  }
-  const handleCheck = id => {
-    const chTasks = [...tasks]
-    setTasks(chTasks.map(item => { 
-      if (item.id === id) {
-        item.check = !item.check
-      }
-      return item
-    })) 
-  }
-  const handleFinish = id => {
-	  const fTasks = [...tasks]
-    setTasks(fTasks.map(item => { 
-      if (item.id === id) {
-        item.finish = !item.finish
-      }
-      return item
-    }))    
-  }
-  const handleAllCheck = check => {
-	  const chTasks = [...tasks]
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const handleAddTd = (data) => {
+    const newTasks = [data, ...tasks];
+    setTasks(newTasks);
+  };
+  const handleDelTd = (id) => {
+    setTasks((prev) => prev.filter((item) => item.id !== id));
+  };
+  const handleCheck = (id) => {
+    const chTasks = [...tasks];
+    setTasks(
+      chTasks.map((item) => {
+        if (item.id === id) {
+          item.check = !item.check;
+        }
+        return item;
+      }),
+    );
+  };
+  const handleFinish = (id) => {
+    const fTasks = [...tasks];
+    setTasks(
+      fTasks.map((item) => {
+        if (item.id === id) {
+          item.finish = !item.finish;
+        }
+        return item;
+      }),
+    );
+  };
+  const handleAllCheck = (check) => {
+    const chTasks = [...tasks];
     for (let i = tasks.length; i--; ) {
-      check ? chTasks[i].check = true : chTasks[i].check = false
+      check ? (chTasks[i].check = true) : (chTasks[i].check = false);
     }
-    setTasks(chTasks.map(item => { 
-      check ? item.check = true : item.check = false
-      return item
-    })) 
-  }	  
+    setTasks(
+      chTasks.map((item) => {
+        check ? (item.check = true) : (item.check = false);
+        return item;
+      }),
+    );
+  };
   const renderList = () => {
     if (tasks.length > 0) {
-      return <List taskData={tasks}/>
+      return (
+        <List
+          taskData={tasks}
+          handleCheck={handleCheck}
+          handleAllCheck={handleAllCheck}
+          handleFinish={handleFinish}
+          handleDelTd={handleDelTd}
+        />
+      );
+    } else {
+      return <div className="NoTasks">No tasks</div>;
     }
-    else {
-      return <div className="NoTasks">No tasks</div>
-    }
-  }
+  };
   return (
-    <Context.Provider value={{
-      handleCheck, 
-      handleAllCheck,
-      handleFinish,
-      handleDelTd
-    }}>
-      <div>	  
-        <Add onAddTd={handleAddTd} />
-        {renderList()}      
-      </div>
-    </Context.Provider>
-  )  
+    <div>
+      <Add onAddTd={handleAddTd} />
+      {renderList()}
+    </div>
+  );
 }
 export default App;
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,72 +1,91 @@
-import React, {useContext} from 'react'
-import './List.css'
-import PropTypes from 'prop-types'
-import {Context} from '../../context'
+import React from 'react';
+import './List.css';
+import PropTypes from 'prop-types';
 
-function List(props) {
-
-  const {handleCheck, handleAllCheck, handleFinish, handleDelTd} = useContext(Context)
-
+function List({ taskData, handleCheck, handleAllCheck, handleFinish, handleDelTd }) {
   const renderButtons = () => {
-    let btn = null
-    if (props.taskData.length > 0) {
-      return <div><button onClick={() => handleAllCheck(true)}>Выбрать все</button><button style={{display: isChecked()}} onClick={() => handleAllCheck(false)}>Снять выделение</button></div>
+    let btn = null;
+    if (taskData.length > 0) {
+      return (
+        <div>
+          <button onClick={() => handleAllCheck(true)}>Выбрать все</button>
+          <button style={{ display: isChecked() }} onClick={() => handleAllCheck(false)}>
+            Снять выделение
+          </button>
+        </div>
+      );
     }
-    return btn 
-  }
+    return btn;
+  };
   const changeAllTasks = () => {
-    for (let i = (props.taskData).length; i--; ) {
-      if ((props.taskData)[i].check) {
-        handleFinish(props.taskData[i].id)
+    for (let i = taskData.length; i--; ) {
+      if (taskData[i].check) {
+        handleFinish(taskData[i].id);
       }
-    }		
-  }
-  const dellAllTasks = () => {
-    for (let i = (props.taskData).length; i--; ) {
-      if ((props.taskData)[i].check) {
-	      handleDelTd(props.taskData[i].id)
-	    }
     }
-  }
+  };
+  const dellAllTasks = () => {
+    for (let i = taskData.length; i--; ) {
+      if (taskData[i].check) {
+        handleDelTd(taskData[i].id);
+      }
+    }
+  };
   const isChecked = () => {
-    let show = 'none'
-    for (let i = (props.taskData).length; i--; ) {
-      if (props.taskData[i].check) { show = 'inline-block'}		
-    } 
-    return show	
-  }
-  return (       
-		<div className="List">	
-		  <div style={{display: isChecked()}}><button onClick={changeAllTasks}>Отметить выполненными</button><button onClick={dellAllTasks}>Удалить</button> </div>
-			<div>{renderButtons()}</div>
+    let show = 'none';
+    for (let i = taskData.length; i--; ) {
+      if (taskData[i].check) {
+        show = 'inline-block';
+      }
+    }
+    return show;
+  };
+  return (
+    <div className="List">
+      <div style={{ display: isChecked() }}>
+        <button onClick={changeAllTasks}>Отметить выполненными</button>
+        <button onClick={dellAllTasks}>Удалить</button>{' '}
+      </div>
+      <div>{renderButtons()}</div>
       <ul>
-        {props.taskData.map(item => {
+        {taskData.map((item) => {
           const cls = ['Task'];
           let chk;
           if (item.finish) {
-            cls.push('Active') 
+            cls.push('Active');
           }
-          item.check ? chk = "checked" : chk = "" 
+          item.check ? (chk = 'checked') : (chk = '');
 
           return (
-            <li className="Task_wrap" key={item.id}> 
+            <li className="Task_wrap" key={item.id}>
               <span>
-                <input type="checkbox" id={item.id} onChange={() => handleCheck(item.id)} checked={chk}/>
+                <input
+                  type="checkbox"
+                  id={item.id}
+                  onChange={() => handleCheck(item.id)}
+                  checked={chk}
+                />
               </span>
               <span className={cls.join(' ')}>
-                <span className="TaskItem" onClick={() => handleFinish(item.id)}>		   
+                <span className="TaskItem" onClick={() => handleFinish(item.id)}>
                   {item.task}
-                </span>			
-                <span className="Del" onClick={() => handleDelTd(item.id)}>X</span>	
+                </span>
+                <span className="Del" onClick={() => handleDelTd(item.id)}>
+                  X
+                </span>
               </span>
             </li>
-          )
+          );
         })}
       </ul>
-		</div>
-  )	
+    </div>
+  );
 }
 List.propTypes = {
   taskData: PropTypes.array.isRequired,
-}
-export default List
+  handleCheck: PropTypes.func.isRequired,
+  handleAllCheck: PropTypes.func.isRequired,
+  handleFinish: PropTypes.func.isRequired,
+  handleDelTd: PropTypes.func.isRequired,
+};
+export default List;
